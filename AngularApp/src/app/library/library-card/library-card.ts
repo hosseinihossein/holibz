d60fc5d4a400c3d89aaaad98399c7b4d6ecb693b
hossein <hossein@mycompany.com>
2025-10-15 18:00:16 +0330

    [Ang] libraries-list component got ready to be set up

--- AngularApp/src/app/library/library-card/library-card.ts
+++ AngularApp/src/app/library/library-card/library-card.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardAvatar, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from "@angular/material/card";
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-library-card',
-  imports: [MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardContent],
+  imports: [MatCard, MatCardHeader, MatCardAvatar, MatCardTitle, MatCardSubtitle, MatCardContent,
+    MatIcon
+  ],
   templateUrl: './library-card.html',
   styleUrl: './library-card.css'
 })
--- AngularApp/src/app/user-account/dashboard/dashboard.ts
+++ AngularApp/src/app/user-account/dashboard/dashboard.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MatSidenav, MatSidenavContainer, MatSidenavContent } from "@angular/material/sidenav";
 import { LibrariesList } from "../../library/libraries-list/libraries-list";
-import { Profile } from "../profile/profile";
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatSidenavContainer, MatSidenav, MatSidenavContent, Profile],
+  imports: [MatSidenavContainer, MatSidenav, MatSidenavContent, LibrariesList, MatButton],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
 export class Dashboard {
-
+  activeSection = signal<null|"MyLibraries">(null);
 }

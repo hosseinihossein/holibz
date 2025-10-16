d348662c7896801293a77f486f21b5c34bf4d86e
hossein <hossein@mycompany.com>
2025-10-16 12:56:34 +0330

    [Asp] Library_DbContext created

--- AspNetCoreApp/Models/Library.cs
+++ AspNetCoreApp/Models/Library.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace AspNetCoreApp.Models;
 
@@ -29,10 +30,16 @@ public class Library_DocumentDbModel
     public string Title { get; set; } = null!;
     public string Description { get; set; } = null!;
     public string Version { get; set; } = "Default";
-    public List<Library_DocumentDbModel> OtherVersions = [];
+    public Library_RelatedVersionsDbModel? RelatedVersions { get; set; }
     public List<Library_ShelfDbModel> Shelves { get; set; } = [];
     public List<Library_ElementDbModel> Elements { get; set; } = [];
     public List<Library_TagDbModel> Tags { get; set; } = [];
+}
+public class Library_RelatedVersionsDbModel
+{
+    public int ID { get; set; }
+    public string Guid { get; set; } = System.Guid.NewGuid().ToString().Replace("-", "");
+    public List<Library_DocumentDbModel> Documents { get; set; } = [];
 }
 public class Library_ElementDbModel
 {
@@ -42,7 +49,8 @@ public class Library_ElementDbModel
     public string Type { get; set; } = null!;
     public string Value { get; set; } = null!;
     public string? Title { get; set; } = null;
-    public List<Library_DocumentDbModel> Document { get; set; } = null!;
+    public int Order { get; set; }
+    public Library_DocumentDbModel Document { get; set; } = null!;
 }
 public class Library_TagDbModel
 {
@@ -50,7 +58,48 @@ public class Library_TagDbModel
     public string Name { get; set; } = null!;
     public List<Library_DocumentDbModel> Documents { get; set; } = [];
 }
+public class Library_DbContext : DbContext
+{
+    public Library_DbContext(DbContextOptions<Library_DbContext> options) : base(options) { }
 
+    public DbSet<Library_LibraryDbModel> Libraries { get; set; } = null!;
+    public DbSet<Library_ShelfDbModel> Shelves { get; set; } = null!;
+    public DbSet<Library_DocumentDbModel> Documents { get; set; } = null!;
+    public DbSet<Library_ElementDbModel> Elements { get; set; } = null!;
+    public DbSet<Library_TagDbModel> Tags { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //*********** Library-Shelves One-To-Many *********
+        modelBuilder.Entity<Library_LibraryDbModel>()
+        .HasMany<Library_ShelfDbModel>(l => l.Shelves)
+        .WithOne(sh => sh.Library)
+        .IsRequired(true);
+
+        //*********** Shelves-Documents Many-To-Many *********
+        modelBuilder.Entity<Library_ShelfDbModel>()
+        .HasMany<Library_DocumentDbModel>(sh => sh.Documents)
+        .WithMany(d => d.Shelves);
+
+        //*********** RelatedVerions-Documents One-To-Many *********
+        modelBuilder.Entity<Library_RelatedVersionsDbModel>()
+        .HasMany<Library_DocumentDbModel>(d => d.Documents)
+        .WithOne(rv => rv.RelatedVersions)
+        .IsRequired(false);
+
+        //*********** Document-Elements One-To-Many *********
+        modelBuilder.Entity<Library_DocumentDbModel>()
+        .HasMany<Library_ElementDbModel>(d => d.Elements)
+        .WithOne(e => e.Document)
+        .IsRequired(true);
+
+        //*********** Tags-Documents Many-To-Many *********
+        modelBuilder.Entity<Library_DocumentDbModel>()
+        .HasMany<Library_TagDbModel>(d => d.Tags)
+        .WithMany(t => t.Documents);
+
+    }
+}
 //********************************************************************************
 public class TagNameBuilder
 {
